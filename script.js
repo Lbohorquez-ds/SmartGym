a7f6e01c617ced180d6723c20c0e78de09132643
Lbohorquez-ds <36065324@ifts24.edu.ar>
2026-06-21 16:27:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuBtn = document.getElementById('menuBtn');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+
+const links = document.querySelectorAll('.menu a');
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('active');
+  });
+});
